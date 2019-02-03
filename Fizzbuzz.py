7fa7af1eb7df9e4237926d9d5e874e02f55268d1
GrayZenith <katyayanib97@gmail.com>
2019-02-03 01:07:24 -0500

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,12 @@
+
+for i in range (1,21):
+    if (i%2 == 0 and i%5 == 0):
+        print "FizzBuzz"
+    elif(i%5 == 0):
+        print "Buzz"
+    elif (i%2==0):
+        print "Fizz"
+    else :
+        print i;
+    
+ 
